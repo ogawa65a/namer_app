9c365425cbef0f737082b83bf6b6c215ec5ce1e3
Takahito Ogawa <takahito.ogawa@vivid-garden.co.jp>
2024-03-17 16:24:58 +0900

    change ListView to GridView

--- lib/main.dart
+++ lib/main.dart
@@ -189,24 +189,41 @@ class FavoritesPage extends StatelessWidget {
     final theme = Theme.of(context);
     var appState = context.watch<MyAppState>();
 
-    return ListView(
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.all(20),
           child: Text('You have '
               '${appState.favorites.length} favorites:'),
         ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: IconButton(
-              icon: Icon(Icons.delete_outline),
-              color: theme.colorScheme.primary,
-              onPressed: () {
-                appState.removeFavorite(pair);
-              },
+        Expanded(
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              childAspectRatio: 400 / 80,
             ),
-            title: Text(pair.asLowerCase),
+            children: [
+              for (var pair in appState.favorites)
+                ListTile(
+                  leading: IconButton(
+                    icon: Icon(Icons.delete_outline),
+                    color: theme.colorScheme.primary,
+                    onPressed: () {
+                      appState.removeFavorite(pair);
+                    },
+                  ),
+                  title: Text(pair.asLowerCase),
+                ),
+            ],
           ),
+        ),
       ],
     );
   }
